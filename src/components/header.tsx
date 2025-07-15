ebbce0f4176f39f4c17e150fdebc67c9b70a3278
harshgarg2110 <harshgarg2110@gmail.com>
2025-07-15 21:38:56 +0530

    changes to homeScreen

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,6 @@ import { cn } from "@/lib/utils";
 import { useAuth } from "@clerk/clerk-react";
 import { Container } from "./container";
 import { LogoContainer } from "./logo-container";
-// import { NavigationRoutes } from "./navigation-routes";
 import { NavLink } from "react-router-dom";
 import { ProfileContainer } from "./profile-container";
 import { ToggleContainer } from "./toggle-container";

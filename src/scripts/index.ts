474d198d5362af0bbd3c67e0a9de8e7e005d374f
harsh garg <harshgarg2110@gmail.com>
2026-05-02 23:12:29 +0530

    change model

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -9,7 +9,8 @@ const apiKey = import.meta.env.VITE_GEMINI_API_KEY!;
 const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({
-  model: "gemini-2.5-pro",
+  // model: "gemini-2.5-pro",
+  model: "gemini-1.5-flash",
 });
 
 

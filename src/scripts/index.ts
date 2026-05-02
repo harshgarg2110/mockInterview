9baa10b3076563cd62e42f3173f7678ad3927a92
harsh garg <harshgarg2110@gmail.com>
2026-05-02 23:28:14 +0530

    update model

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -10,7 +10,7 @@ const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({
   // model: "gemini-2.5-pro",
-  model: "gemini-1.5-flash",
+  model: "gemini-1.5-flash-latest",
 });
 
 
